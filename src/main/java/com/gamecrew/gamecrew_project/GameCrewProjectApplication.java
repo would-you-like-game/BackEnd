865efb794a1c83887601ba28618aa3f306f5af0c
chaewon <cwlee4633@naver.com>
2023-10-12 10:52:16 +0900

    first commit

---
+++ src/main/java/com/gamecrew/gamecrew_project/GameCrewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gamecrew.gamecrew_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameCrewProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameCrewProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gamecrew/gamecrew_project/GameCrewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamecrew.gamecrew_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameCrewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
