234216cc8d60385f2aaf8fdeecc1415ec8c98570
chaewon <cwlee4633@naver.com>
2023-10-21 14:18:25 +0900

    feat : Email Authentication
    body : 403에러 - 수정해야 함

--- src/main/java/com/gamecrew/gamecrew_project/domain/auth/controller/AuthController.java
+++ src/main/java/com/gamecrew/gamecrew_project/domain/auth/controller/AuthController.java
@@ -2,26 +2,32 @@ package com.gamecrew.gamecrew_project.domain.auth.controller;
 
 import com.gamecrew.gamecrew_project.domain.auth.dto.request.LoginRequestDto;
 import com.gamecrew.gamecrew_project.domain.auth.service.AuthService;
-import com.gamecrew.gamecrew_project.global.response.MessageResponseDto;
-import com.gamecrew.gamecrew_project.global.response.constant.Message;
+import com.gamecrew.gamecrew_project.domain.auth.service.EmailService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthService authService;
+    private final EmailService emailService;
 
     @PostMapping("/login")
     public void login(@RequestBody @Valid LoginRequestDto requestDto, HttpServletResponse res){
         authService.login(requestDto, res);
+    }
+
+    @PostMapping("/signup/email/check")
+    @ResponseBody
+    public String mailConfirm(@RequestParam String email) throws Exception {
+        String code = emailService.sendSimpleMessage(email);
+        log.info("인증코드 : " + code);
+        return code;
     }
 }
