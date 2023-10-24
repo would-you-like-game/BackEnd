2d27fa6124912c0c1ba9399e5dfc0739234f89a7
chaewon <cwlee4633@naver.com>
2023-10-24 13:21:09 +0900

    feat : WebSocket Chat function
    body : 채팅방 생성 및 전체 채팅방 목록 조회,유저 인증 기능 전, 에러 상태코드 400 -> 401로 변경

---
+++ src/main/java/com/gamecrew/gamecrew_project/domain/chat/controller/ChatController.java
@@ -0,0 +1,29 @@
+package com.gamecrew.gamecrew_project.domain.chat.controller;
+
+import com.gamecrew.gamecrew_project.domain.chat.dto.ChatRoom;
+import com.gamecrew.gamecrew_project.domain.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService service;
+
+    @PostMapping("/{userAId}/{userBId}")
+    public ChatRoom createRoom(@PathVariable String userAId, @PathVariable String userBId){
+        return service.createRoom(userAId, userBId);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRooms(){
+        return service.findAllRoom();
+    }
+}
+
