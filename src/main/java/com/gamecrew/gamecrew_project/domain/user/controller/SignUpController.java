de283e23db30b6d09a933b9876038b2e5d054aa6
lodalee <131640279+lodalee@users.noreply.github.com>
2023-10-25 15:14:27 +0900

    Update SignUpController.java

--- src/main/java/com/gamecrew/gamecrew_project/domain/user/controller/SignUpController.java
+++ src/main/java/com/gamecrew/gamecrew_project/domain/user/controller/SignUpController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@CrossOrigin(origins = "*", allowHeaders = "*")
 public class SignUpController {
     private final SignUpService signupService;
 
