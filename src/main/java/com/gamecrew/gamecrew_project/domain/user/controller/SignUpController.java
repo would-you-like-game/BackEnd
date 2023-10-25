f4ec6b2b0867d6fb362bd9191b938fdedcbec5c9
chaewon <cwlee4633@naver.com>
2023-10-25 15:16:43 +0900

    docs : cors 에러
    body : @CrossOrigin(origins = "*", allowedHeaders = "*") 추가

--- src/main/java/com/gamecrew/gamecrew_project/domain/user/controller/SignUpController.java
+++ src/main/java/com/gamecrew/gamecrew_project/domain/user/controller/SignUpController.java
@@ -1,5 +1,6 @@
 package com.gamecrew.gamecrew_project.domain.user.controller;
 
+import com.gamecrew.gamecrew_project.domain.user.service.EmailService;
 import com.gamecrew.gamecrew_project.domain.user.dto.request.CheckEmailRequestDto;
 import com.gamecrew.gamecrew_project.domain.user.dto.request.CheckNicknameRequestDto;
 import com.gamecrew.gamecrew_project.domain.user.dto.request.SignupRequestDto;
@@ -9,16 +10,18 @@ import com.gamecrew.gamecrew_project.global.response.MessageResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SignUpController {
     private final SignUpService signupService;
+    private final EmailService emailService;
 
     @PostMapping("/signup")
     public MessageResponseDto signup(@RequestBody @Valid SignupRequestDto requestDto){
@@ -38,5 +41,35 @@ public class SignUpController {
     public MessageResponseDto checkEmail(@RequestBody @Valid CheckEmailRequestDto requestDto) {
         signupService.checkEmail(requestDto.getEmail());
         return new MessageResponseDto(Message.AVAILABLE_EMAIL, HttpStatus.OK);
+    }
+
+    private int number; // 이메일 인증 숫자를 저장하는 변수
+
+    // 인증 이메일 전송
+    @PostMapping("/mailSend")
+    public HashMap<String, Object> mailSend(String mail) {
+        HashMap<String, Object> map = new HashMap<>();
+
+        try {
+            number = emailService.sendMail(mail);
+            String num = String.valueOf(number);
+
+            map.put("success", Boolean.TRUE);
+            map.put("number", num);
+        } catch (Exception e) {
+            map.put("success", Boolean.FALSE);
+            map.put("error", e.getMessage());
+        }
+
+        return map;
+    }
+
+    // 인증번호 일치여부 확인
+    @GetMapping("/mailCheck")
+    public ResponseEntity<?> mailCheck(@RequestParam String userNumber) {
+
+        boolean isMatch = userNumber.equals(String.valueOf(number));
+
+        return ResponseEntity.ok(isMatch);
     }
 }
