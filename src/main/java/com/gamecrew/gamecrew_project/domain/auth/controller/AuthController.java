20456bb659483a8e007a045508aeadbefab92c17
lodalee <131640279+lodalee@users.noreply.github.com>
2023-10-25 15:27:26 +0900

    Update AuthController.java

--- src/main/java/com/gamecrew/gamecrew_project/domain/auth/controller/AuthController.java
+++ src/main/java/com/gamecrew/gamecrew_project/domain/auth/controller/AuthController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthController {
 
     private final AuthService authService;
