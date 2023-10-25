688c490beb5058629e9c3a574378ad25e26afe9e
lodalee <131640279+lodalee@users.noreply.github.com>
2023-10-25 15:03:55 +0900

    Update WebSecurity.java

--- src/main/java/com/gamecrew/gamecrew_project/global/config/WebSecurity.java
+++ src/main/java/com/gamecrew/gamecrew_project/global/config/WebSecurity.java
@@ -17,6 +17,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
@@ -63,7 +65,7 @@ public class WebSecurity {
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
                         .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
-                        .requestMatchers("/user/**", "/auth/login", "/chat/**").permitAll() // '/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/user/**", "/auth/**", "/chat/**").permitAll() // '/user/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
@@ -72,5 +74,18 @@ public class WebSecurity {
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+                    .maxAge(3600);
+        }
     }
 }
