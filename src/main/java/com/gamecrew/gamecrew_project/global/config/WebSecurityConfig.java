66a72561ce82f3ae6237289dde236e5dabbf1eea
lodalee <131640279+lodalee@users.noreply.github.com>
2023-10-31 11:50:46 +0900

    프론트 https cors 허용

--- src/main/java/com/gamecrew/gamecrew_project/global/config/WebSecurityConfig.java
+++ src/main/java/com/gamecrew/gamecrew_project/global/config/WebSecurityConfig.java
@@ -78,6 +78,7 @@ public class WebSecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://d1ho26p2hfntk1.cloudfront.net/"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
