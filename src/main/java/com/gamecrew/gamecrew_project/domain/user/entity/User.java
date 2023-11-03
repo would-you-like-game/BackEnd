a047e830fea4c345894931b68a0aa4059c54f5fd
chaewon <cwlee4633@naver.com>
2023-11-03 20:27:35 +0900

    feat : 유저 프로필 조회 기능

--- src/main/java/com/gamecrew/gamecrew_project/domain/user/entity/User.java
+++ src/main/java/com/gamecrew/gamecrew_project/domain/user/entity/User.java
@@ -24,9 +24,13 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    public User(String email, String nickname, String password) {
+    @Column
+    private String userImg;
+
+    public User(String email, String nickname, String password, String userImg) {
         this.email = email;
         this.nickname = nickname;
         this.password = password;
+        this.userImg = userImg;
     }
 }
