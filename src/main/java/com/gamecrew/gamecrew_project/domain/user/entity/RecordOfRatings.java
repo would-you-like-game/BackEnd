6998e055eaad05c7ffb421513d4ed0e069977fc4
chaewon <cwlee4633@naver.com>
2023-11-20 23:10:25 +0900

    refactor : 유저 평점 반영 변경 body : 최근의 점수 최대 10개까지 반영하여 이를 더해 나누어준다.

--- src/main/java/com/gamecrew/gamecrew_project/domain/user/entity/RecordOfRatings.java
+++ src/main/java/com/gamecrew/gamecrew_project/domain/user/entity/RecordOfRatings.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -39,7 +42,11 @@ public class RecordOfRatings{
     @Column(nullable = false)
     private double totalRating;
 
-    public RecordOfRatings(Long userId, Long evaluator, int manner, int participation, int gamingSkill, int enjoyable, int sociability, double totalRating) {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime recordedAt;
+
+    public RecordOfRatings(Long userId, Long evaluator, int manner, int participation, int gamingSkill, int enjoyable, int sociability, double totalRating, LocalDateTime recordedAt) {
         this.userId = userId;
         this.evaluator = evaluator;
         this.manner = manner;
@@ -48,6 +55,7 @@ public class RecordOfRatings{
         this.enjoyable = enjoyable;
         this.sociability = sociability;
         this.totalRating = Math.round(totalRating * 2) / 2.0;
+        this.recordedAt = recordedAt;
     }
 }
 
--- src/main/java/com/gamecrew/gamecrew_project/domain/user/repository/RecordOfRatingsRepository.java
+++ src/main/java/com/gamecrew/gamecrew_project/domain/user/repository/RecordOfRatingsRepository.java
@@ -1,9 +1,14 @@
 package com.gamecrew.gamecrew_project.domain.user.repository;
 
 import com.gamecrew.gamecrew_project.domain.user.entity.RecordOfRatings;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface RecordOfRatingsRepository extends JpaRepository<RecordOfRatings, Long> {
 
     Long countByUserId(Long userId);
+
+    List<RecordOfRatings> findTop9ByUserIdOrderByRecordedAtDesc(Long userId, PageRequest pageable);
 }
